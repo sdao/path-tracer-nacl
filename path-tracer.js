0a9af509207ab85374da6cb4ed2de053ebc2d668
Steven Dao <stevedao@gmail.com>
2015-03-20 11:43:55 -0500

    timing in JS

--- path-tracer.js
+++ path-tracer.js
@@ -1,3 +1,9 @@
+var startTime = new Date();
+
 function handleMessage(message) {
   document.getElementById('iterationCount').textContent = message.data;
+
+  var curTime = new Date();
+  var diff = Math.round((curTime - startTime) / 1000);
+  document.getElementById('timer').textContent = diff + " seconds";
 }
